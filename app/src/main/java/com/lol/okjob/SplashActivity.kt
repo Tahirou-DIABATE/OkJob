6d9b05c8feca022066668270c6d5e180eb45232d
Tahirou DIABATE <tahiroudiabate2@gmail.com>
2020-01-18 13:34:12 +0000

    AppIntro & SplashScreen ok

--- app/src/main/java/com/lol/okjob/SplashActivity.kt
+++ app/src/main/java/com/lol/okjob/SplashActivity.kt
@@ -3,9 +3,6 @@ package com.lol.okjob
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
-import android.view.Window.FEATURE_NO_TITLE
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
